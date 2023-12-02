b7bcf0796de12bd4c94acc96b0ff2e14ce974803
edsonibarra <edsonibarra97@gmail.com>
2023-12-02 11:57:07 -0600

    added comments

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import pandas as pd
 import sqlite3
 
 
+# Step 1: Read the CSV file into a Pandas DataFrame
 csv_file_path = "netflix_titles.csv"
 df = pd.read_csv(csv_file_path)
 
+# Step 2: Connect to the SQLite database
 db_path = "netflix_titles.sqlite"
 conn = sqlite3.connect(db_path)
 
+# Step 3: Write the DataFrame to the SQLite database
 table_name = "movies"
 df.to_sql(table_name, conn, index=False, if_exists="replace")
 
+# Step 4: Close the connection
 conn.close()
 print("Data imported")
